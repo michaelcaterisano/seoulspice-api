1c5977703a5a570fbbc081a981fe2722f6cc97e1
Michael Caterisano <mcaterisano@gmail.com>
2020-12-27 21:02:37 -0500

    sending orderId and orderTotal to client

--- controllers/createOrderController.js
+++ controllers/createOrderController.js
@@ -1,12 +1,14 @@
 const orderService = require("../services/orderService");
 const OrderBuilder = require("../models/OrderBuilder");
+const chalk = require("chalk");
 
 const createOrder = async (req, res, next) => {
   try {
     const data = req.body;
     const order = new OrderBuilder(data).getOrder();
-    const result = await orderService.createOrder(order);
-    return res.send(result);
+    const response = await orderService.createOrder(order, next);
+    const { id, totalMoney } = response.result.order;
+    return res.send({ orderId: id, orderTotal: totalMoney.amount });
   } catch (error) {
     next(error);
   }
--- services/orderService.js
+++ services/orderService.js
@@ -1,12 +1,12 @@
 const client = require("./squareClient");
 const chalk = require("chalk");
 
-const createOrder = async (order) => {
+const createOrder = async (order, next) => {
   try {
     const result = await client.ordersApi.createOrder(order);
     return result;
   } catch (error) {
-    throw new Error(chalk.red(JSON.stringify(error, null, 2)));
+    next(error);
   }
 };
 
