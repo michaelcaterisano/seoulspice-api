5459e2d7bd6247bc5ac1bcccdd5585874803fbfb
Michael Caterisano <mcaterisano@gmail.com>
2020-12-29 14:05:58 -0500

    adding health route

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,10 @@ router.options("/", (req, res) => {
   res.status(204).send("ok");
 });
 
+router.get("/", (req, res) => {
+  res.send(`${process.env.NODE_ENV} API is running`);
+});
+
 router.post("/create-order", createOrder);
 
 router.post("/add-loyalty-account", addLoyaltyAccount);
