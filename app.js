e6414769dad89f5fd3d5ad90e7b2181b27cff4ec
Michael Caterisano <mcaterisano@gmail.com>
2021-01-11 11:15:20 -0500

    adding Sentry for error logging

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const Sentry = require("@sentry/node");
+const Tracing = require("@sentry/tracing");
 const fs = require("fs");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
@@ -8,14 +10,18 @@ const { Console } = require("console");
 const app = express();
 const port = process.env.PORT || 3000;
 
-// if (!module.parent) { <--- for testing
-// }
-// TODO:
-// set cookie stuff
-// set uuid
-// if cookie, rate limit
-
-// body parsing
+Sentry.init({
+  dsn:
+    "https://292e0f7beb6646b7ab460f01ab15cd1d@o503252.ingest.sentry.io/5588106",
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Tracing.Integrations.Express({ app }),
+  ],
+  // percentage of transactions sent to sentry, between 0 and 1
+  tracesSampleRate: 1.0,
+});
 
 if (
   process.env.NODE_ENV === "development" ||
@@ -32,10 +38,20 @@ app.use(
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(Sentry.Handlers.requestHandler());
+  app.use(Sentry.Handlers.tracingHandler());
+}
+
 app.use("/", router);
-app.get("*", (req, res) => {
-  res.status(404).send();
-});
+
+// app.get("*", (req, res) => {
+//   res.status(404).send();
+// });
+if (process.env.NODE_ENV === "production") {
+  app.use(Sentry.Handlers.errorHandler());
+}
 
 // error handling
 app.use((error, req, res, next) => {
