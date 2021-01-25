249d88e59462112ce8c7f8628155af939beead2f
Michael Caterisano <mcaterisano@gmail.com>
2021-01-25 10:11:51 -0500

    adding winston file rotation

--- config/winston.js
+++ config/winston.js
@@ -1,5 +1,6 @@
 const { createLogger, format, transports } = require("winston");
 const { combine, timestamp, label, printf, json, simple } = format;
+require("winston-daily-rotate-file");
 
 const myFormat = printf(({ message, data, timestamp }) => {
   return `${timestamp} ${message} ${data}`;
@@ -8,30 +9,23 @@ const myFormat = printf(({ message, data, timestamp }) => {
 const getTimestamp = () => {
   var tzoffset = new Date().getTimezoneOffset() * 60000; //offset in milliseconds
   var localISOTime = new Date(Date.now() - tzoffset).toISOString().slice(0, -1);
-  // return new Date().toLocaleString();
   return localISOTime;
 };
+
+const transport = new transports.DailyRotateFile({
+  filename: "./logs/activity-%DATE%.log",
+  datePattern: "YYYY-MM-DD-HH",
+  zippedArchive: true,
+  maxSize: "20m",
+  maxFiles: "14d",
+});
 
 const options = {
   level: "info",
   format: combine(timestamp({ format: getTimestamp() }), json(), myFormat),
-  transports: [
-    new transports.File({
-      filename: "./logs/error.log",
-      level: "error",
-    }),
-    new transports.File({ filename: "./logs/combined.log" }),
-  ],
+  transports: [transport],
 };
 
 const logger = new createLogger(options);
-
-// if (process.env.NODE_ENV !== "production") {
-//   logger.add(
-//     new transports.Console({
-//       format: simple(),
-//     })
-//   );
-// }
 
 module.exports = logger;
