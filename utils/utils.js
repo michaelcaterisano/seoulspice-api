2031394b84450ccf9cf57363d689ff0054517107
Michael Caterisano <mcaterisano@gmail.com>
2021-03-03 19:10:03 -0500

    Add authenticated menu endpoints (closes #35)
    * add menu route and jwt check
    
    * wip: adding enpoints for menu db
    
    * adding get ingredients and get locations controllers
    
    * add controller for out of stock ingredients
    
    * adding menu filter
    
    * wip: working on filter function
    
    * wip: choices filter acting funny
    
    * menu filter working

--- utils/utils.js
+++ utils/utils.js
@@ -9,4 +9,34 @@ const getTimestamp = () => {
   return new Date().toLocaleString("en-US", { timeZone: "America/New_York" });
 };
 
-module.exports = { phoneNumberIsValid, getTimestamp };
+const filterByTag = (arr, ...tags) => {
+  return arr
+    .filter(
+      (item) =>
+        !item.tags ||
+        !item.tags.length ||
+        item.tags.some((tag) => !tags.includes(tag))
+    )
+    .map((item) => {
+      if (item.choices) {
+        return {
+          ...item,
+          choices: item.choices.filter(
+            (choice) =>
+              !choice.tags.length ||
+              choice.tags.some((tag) => !tags.includes(tag))
+          ),
+        };
+      } else return item;
+    });
+};
+
+const filterMenu = (menuData, outOfStock) => {
+  let filteredMenu = {};
+  Object.keys(menuData).forEach((key) => {
+    filteredMenu[key] = filterByTag(menuData[key], ...outOfStock);
+  });
+  return filteredMenu;
+};
+
+module.exports = { phoneNumberIsValid, getTimestamp, filterMenu };
